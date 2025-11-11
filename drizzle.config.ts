19112091d2b0732366808b7012079582f76c1b21
yuv-raj-25 <yuvrajchaudhary464@gmail.com>
2025-11-11 16:31:19 +0530

    drizzle config added

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import { config } from 'dotenv';
+config({ path: '.env.local' });
+
+import type { Config } from "drizzle-kit";
+
+export default {
+  schema: "./db/schema.ts",
+  out: "./db/migrations",
+  dialect: "turso",
+  dbCredentials: {
+    url: process.env.TURSO_DATABASE_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN,
+  },
+} satisfies Config;
