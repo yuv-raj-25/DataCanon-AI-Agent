0457ad0f9d71b62644d0c3ee2dfbc6dc7a2388c9
yuv-raj-25 <yuvrajchaudhary464@gmail.com>
2025-11-11 16:47:19 +0530

    llm added for the query into the database

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,72 +1,74 @@
+import { db } from '@/db/db';
 import { openai } from '@ai-sdk/openai';
-import { streamText, UIMessage, convertToModelMessages , tool} from 'ai';
-import { z } from 'zod';
+import { streamText, UIMessage, convertToModelMessages, tool, stepCountIs } from 'ai';
+import z from 'zod';
 
+// Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
 
-export async function GET() {
-  return new Response('Chat endpoint is live ✅', { status: 200 });
-}
-
 export async function POST(req: Request) {
-  try {
     const { messages }: { messages: UIMessage[] } = await req.json();
-    //   console.log('✅ Received messages:', JSON.stringify(messages, null, 2))
 
-    const SYSTEM_PROMPT = `You are an SQL assistant that helps user to query their database using natural language.
+    const SYSTEM_PROMPT = `You are an expert SQL assistant that helps users to query their database using natural language.
+
+    ${new Date().toLocaleString('sv-SE')}
     You have access to following tools:
     1. db tool - call this tool to query the database.
+    2. schema tool - call this tool to get the database schema which will help you to write sql query.
 
-    Rules:
-    - Generate ONLY SELECT queries (no INSERT , UPDATE , DELETE, DROP).
-    - Return valid SQLite syntax
+Rules:
+- Generate ONLY SELECT queries (no INSERT, UPDATE, DELETE, DROP)
+- Always use the schema provided by the schema tool
+- Pass in valid SQL syntax in db tool.
+- IMPORTANT: To query database call db tool, Don't return just SQL query.
 
-    Always respond in a helpfull, conversational tone while being technically accurate. 
-
-    `;
-//    1. Schema tool - call this tool to get the database schema which will help you to write accurate SQL queries.
-//  - Always use the schema provided by the schema tool 
+Always respond in a helpful, conversational tone while being technically accurate.`;
 
     const result = streamText({
-      model: openai('gpt-4o'),
-      messages: convertToModelMessages(messages),
-      system: SYSTEM_PROMPT,
-      tools: {
-      db: tool({
-        description: 'call this tool to query a database',
-        inputSchema: z.object({
-          query: z.string().describe('the SQL query '),
-        }),
-        execute: async ({ query }) => {
-            console.log("Query", query);
-            return '';
-        },
-      }),
-      convertFahrenheitToCelsius: tool({
-        description: 'Convert a temperature in fahrenheit to celsius',
-        inputSchema: z.object({
-          temperature: z
-            .number()
-            .describe('The temperature in fahrenheit to convert'),
-        }),
-        execute: async ({ temperature }) => {
-          const celsius = Math.round((temperature - 32) * (5 / 9));
-          return {
-            celsius,
-          };
-        },
-      }),
-    },
-    });
+        model: openai('gpt-5-nano-2025-08-07'),
+        messages: convertToModelMessages(messages),
+        system: SYSTEM_PROMPT,
+        stopWhen: stepCountIs(5),
+        tools: {
+            schema: tool({
+                description: 'Call this tool to get database schema information.',
+                inputSchema: z.object({}),
+                execute: async () => {
+                    return `CREATE TABLE products (
+	id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	name text NOT NULL,
+	category text NOT NULL,
+	price real NOT NULL,
+	stock integer DEFAULT 0 NOT NULL,
+	created_at text DEFAULT CURRENT_TIMESTAMP
+)
 
+CREATE TABLE sales (
+	id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	product_id integer NOT NULL,
+	quantity integer NOT NULL,
+	total_amount real NOT NULL,
+	sale_date text DEFAULT CURRENT_TIMESTAMP,
+	customer_name text NOT NULL,
+	region text NOT NULL,
+	FOREIGN KEY (product_id) REFERENCES products(id) ON UPDATE no action ON DELETE no action
+)`;
+                },
+            }),
+            db: tool({
+                description: 'Call this tool to query a database.',
+                inputSchema: z.object({
+                    query: z.string().describe('The SQL query to be ran.'),
+                }),
+                execute: async ({ query }) => {
+                    console.log('Query', query);
+                    // Important: make sure you sanitize / validate (somehow) check the query
+                    // string search [delete, update] -> Guardrails
+                    return await db.run(query);
+                },
+            }),
+        },
+    });
 
     return result.toUIMessageStreamResponse();
-  } catch (err) {
-    console.error('❌ Chat route error:', err);
-    return new Response(JSON.stringify({ error: 'AI call failed' }), {
-      status: 500,
-      headers: { 'Content-Type': 'application/json' },
-    });
-    
-  }
 }
